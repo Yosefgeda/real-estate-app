d22cc5acc3e8f4474440d542f5c1b05ba5b7c527
Yosefgeda <100337086+Yosefgeda@users.noreply.github.com>
2023-12-11 20:58:45 +0300

    Created Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Signin from './pages/Signin';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
 
 const App = () => {
   return (
-    <div className='text-red-400'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={ <Home /> } />
+        <Route path='/about' element={ <About /> }/>
+        <Route path='/signin' element={ <Signin /> }/>
+        <Route path='/signup' element={ <SignUp /> }/>
+        <Route path='/profile' element={ <Profile /> }/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
